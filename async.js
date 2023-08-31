218fa6a3483d5a8738880609ef6c7156709b14f9
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-08-31 19:40:52 +0530

    Initial code in async and promise

---
+++ async.js
@@ -0,0 +1,38 @@
+console.log('person1 : shows ticket');
+console.log('person2 : shows ticket');
+
+const preMovie = async () =>{
+    const wifeBringingTicket = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve('Ticket')
+        },3000)
+    })
+
+    const getPopcorn = new Promise((resolve,reject)=> resolve('Popcorn'))
+    const addbutter = new Promise((resolve,reject)=>resolve('Butter'))
+
+    const ticket = await wifeBringingTicket;
+    console.log('wife : here are the tickets');
+    console.log('husband : we should go in');
+    console.log('wife : I am hungry');
+    console.log('husband : I will get something to eat');
+    const popcorn = await getPopcorn;
+    console.log('husband : I got you some popcorn')
+    console.log('wife : I want butter on popcorn');
+    console.log('husband : okay')
+    const butter = await addbutter;
+    console.log('husband : here you go')
+    console.log('wife : lets go in')
+
+    return `person3 : shows ${ticket} ${popcorn} ${butter}`
+
+
+
+}
+
+preMovie().then((m)=>{
+    console.log(m)
+})
+
+console.log('person4 : shows ticket');
+console.log('person5 : shows ticket');
---
+++ promise.js
@@ -0,0 +1,39 @@
+console.log('person1 : shows ticket');
+console.log('person2 : shows ticket');
+
+const wifeBringingTicket = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('ticket')
+    },3000)
+})
+
+const getPopcorn = wifeBringingTicket.then((t)=>{
+    console.log('wife : here are the tickets');
+    console.log('husband : we should go in');
+    console.log('wife : I am hungry');
+    console.log('husband : I will get something to eat');
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Husband : Here, have some popcorn')
+            resolve(`popcorn ${t}`)
+        },3000)
+    })
+})
+
+const getButter=getPopcorn.then((t)=>{
+    console.log('wife : I want butter on popcorn');
+    console.log('husband : okay')
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log('husband : here you go')
+            resolve(`butter ${t}`)
+        }, 3000);
+    })
+})
+
+getButter.then((t)=>{
+    console.log(`person3 : shows ${t}`)
+})
+
+console.log('person4 : shows ticket');
+console.log('person5 : shows ticket');
