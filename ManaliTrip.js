21d26010588e7a6879aecdd3d91a5eabefd72ae6
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-08-31 23:11:53 +0530

    Manali-async/await

--- ManaliTrip.js
+++ ManaliTrip.js
@@ -1,33 +1,33 @@
-function buyCar(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            resolve('bought a car');
-        }, 5000);
-    })
-}
 
-function planTrip(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            resolve('Lets go to manali');
-        }, 3000);
-    })
-}
-
-function travel(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            resolve('Reached manali')
-        }, 2000);
-    })
-}
-
-buyCar().then((val)=>{
-    console.log(val);
-    planTrip().then((val)=>{
-        console.log(val)
-        travel().then((val)=>{
-            console.log(val)
+let trip=async ()=>{
+    function buyCar(){
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                resolve('bought a car');
+            }, 5000);
         })
-    })
-})
+    }
+    
+    function planTrip(){
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                resolve('Lets go to manali');
+            }, 3000);
+        })
+    }
+    
+    function travel(){
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                resolve('Reached manali')
+            }, 2000);
+        })
+    }
+    
+    let car=await buyCar();
+    let place=await planTrip();
+    let journey=await travel();
+
+    return `${car}\n${place}\n${journey}`
+}
+trip().then((val)=>console.log(val))
